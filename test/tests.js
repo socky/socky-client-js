51d0cb60bbfc7c44cbefd12955ed96ccf325d78c
Bernard Potocki <bernard.potocki@imanel.org>
2011-04-03 00:08:34 +0200

    First tests

---
+++ test/tests.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+  
+  //// Connection
+  
+  // connection_established
+  var socky_connection_established = new Socky('ws://localhost:3001/websocket/my_app', {assets_location: '../dist/0.5.0-pre/assets'});
+  socky_connection_established.bind("socky:connection:established", function(payload) {
+    if(payload.connection_id != undefined) {
+      pass('connection_established');
+      socky_connection_established.close();
+    };
+  });
+  
+  // connection_error_refused
+  var socky_connection_error_refused = new Socky('ws://localhost:3001/websocket/invalid_app', {assets_location: '../dist/0.5.0-pre/assets'});
+  socky_connection_error_refused.bind("socky:connection:error", function(payload) {
+    if(payload.reason == 'refused') {
+      pass('connection_error_refused');
+      socky_connection_error_refused.close();
+    };
+  });
+  
+  // connection_error_down
+  var socky_connection_error_down = new Socky('ws://example.com:3001/websocket/my_app', {assets_location: '../dist/0.5.0-pre/assets'});
+  socky_connection_error_down.bind("socky:connection:error", function(payload) {
+    if(payload.reason == 'down') {
+      pass('connection_error_down');
+      socky_connection_error_down.close();
+    };
+  });
+  
+  // connection_closed
+  var socky_connection_closed = new Socky('ws://localhost:3001/websocket/my_app', {assets_location: '../dist/0.5.0-pre/assets'});
+  socky_connection_closed.bind("socky:connection:closed", function(payload) {
+    pass('connection_closed');
+  });
+  socky_connection_closed.close();
+  
+  //// Public channel
+  
+  // public_subscribe_success
+  var socky_public_subscribe_success = new Socky('ws://localhost:3001/websocket/my_app', {assets_location: '../dist/0.5.0-pre/assets'});
+  socky_public_subscribe_success.bind("socky:subscribe:success", function(payload) {
+    if(payload.channel == 'public_subscribe_success') {
+      pass('public_subscribe_success');
+      socky_public_subscribe_success.close();
+    };
+  });
+  socky_public_subscribe_success.subscribe('public_subscribe_success');
+  
+});
