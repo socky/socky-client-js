64d491a0cd14fce18f35fe35ac8d611090c9451a
Bernard Potocki <bernard.potocki@imanel.org>
2011-08-02 09:52:23 +0200

    Update new repo location in license

--- src/socky-licence.js
+++ src/socky-licence.js
@@ -5,5 +5,5 @@
  * @author  Bernard Potocki <bernard.potocki@imanel.org>
  * @author  Stefano Verna <stefano.verna@welaika.com>
  * @licence The MIT licence.
- * @source  http://github.com/socky/socky-js
+ * @source  http://github.com/socky/socky-client-js
  */
